979f0c8db75abfa9d821fefac193f1aa5604266e
WuEcho <emailforecho@163.com>
2024-02-19 14:23:58 +0800

    Update init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -238,14 +238,11 @@ install_direnv(){
     fi
     
     if [ "$ACTION" == "init env" ];then
-        if [ "$NODETYPE" == "d_sequencer" ] || [ "$NODETYPE" == "op_sequencer" ]; then
-            echo 'NOTE!!! You should do !!!:  1.Do source /home/ubuntu/.bashrc. When initenv.sh is finished.  2.Input: "curl -sfL https://direnv.net/install.sh | bash" do the Direnv Installation.Conf was written into /home/ubuntu/.bashrc file. Please do  source /home/ubuntu/.bashrc again. 3.Input foundryup in the terminal to finished Foundry Installation.'
-        elif [ "$NODETYPE" == "d_normal" ] || [ "$NODETYPE" == "op_normal" ];then
-            echo 'NOTE!!! You should do !!!: 1.Input: "curl -sfL https://direnv.net/install.sh | bash" do the Direnv Installation.Conf was written into /home/ubuntu/.bashrc file. Please do source /home/ubuntu/.bashrc.'
-            
-        fi
-        
+        echo
+        echo 'NOTE!!! You should do !!!:  1.Do source /home/ubuntu/.bashrc. When initenv.sh is finished.  2.Input: "curl -sfL https://direnv.net/install.sh | bash" do the Direnv Installation. Conf was written into /home/ubuntu/.bashrc file. Please do  source /home/ubuntu/.bashrc again. 3.Input: "foundryup" in the terminal to finished Foundry Installation.'
+       
         echo "You can run versions.sh to check version"
+        echo
     fi
 }
 
